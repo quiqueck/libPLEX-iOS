c92487f71833b6335a1f122577ccb9c8f7ea8831
Frank <frank@ambertation.de>
2011-05-01 17:21:34 +0200

    Fixed: runtime error due to missing PlxProvider

---
+++ include/plex-oss/PlexProvider.h
@@ -0,0 +1,16 @@
+//
+//  PlexProvider.h
+//  PlexPad
+//
+//  Created by Frank Bauer on 30.04.11.
+//  Copyright 2011 Ambertation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PlexMediaObject.h"
+
+@interface PlexProvider : PlexMediaObject {
+    
+}
+
+@end
